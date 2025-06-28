e14ebbc82910f44c06c0042cbb27e196036f90af
Vinayak <145529911+Vinayak2005917@users.noreply.github.com>
2025-06-28 12:54:03 +0530

    corrected linear_search.py

--- linear_search.py
+++ linear_search.py
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 def linear(list, target):
     for i in list:
         for j in i:
             if j == target:
                 return i
 
-=======
-def linear(list, target):
-    for i in list:
-        for j in i:
-            if j == target:
-                return i
-
->>>>>>> 42fbb5591b1388223be4a49e50bf919dc409d819
